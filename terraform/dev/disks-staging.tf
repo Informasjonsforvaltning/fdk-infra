b8517ccdc0e9419a1761162009619b968d4c998e
Hans Olav Sund <hans.olav.sund@digdir.no>
2026-06-18 15:14:57 +0200

    chore: drop unused resource-service disk

--- terraform/dev/disks-staging.tf
+++ terraform/dev/disks-staging.tf
@@ -68,17 +68,6 @@ resource "google_compute_disk" "staging_reports_store" {
   zone                      = var.zone
 }
 
-resource "google_compute_disk" "staging_resource_service" {
-  labels = local.staging_disk_labels
-
-  name                      = "${var.disk_prefix}-staging-resource-service"
-  physical_block_size_bytes = 4096
-  project                   = var.project_id
-  size                      = 20
-  type                      = "pd-standard"
-  zone                      = var.zone
-}
-
 resource "google_compute_disk" "staging_static_rdf_server" {
   labels = local.staging_disk_labels
 
